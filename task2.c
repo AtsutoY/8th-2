6ecb904a15d117ebb1aa7c5d6f53d0d56cadd12a
Atsuto <cmt14137@ict.nitech.ac.jp>
2023-06-11 16:13:53 +0900

    change equation

--- task2.c
+++ task2.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-    int tall;
-    printf("身長を入力せよ:");  scanf("%d",&tall);
-    printf("標準体重は%.1fです。\n",(tall-100)*0.9);
+    int height;
+    printf("身長を入力せよ:");
+    scanf("%d", &height);
+    printf("標準体重は%.1fです。\n", (height - 100) * 0.9);
 }
